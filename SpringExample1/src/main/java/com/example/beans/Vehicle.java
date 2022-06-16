5e12a7c38a236a0f6860bb54f3208a8fbadf6568
Praveen Chennupalli <51333822+chpraveen2905@users.noreply.github.com>
2022-06-15 23:58:52 -0400

    SpringExample1
    Adding new beans to the Spring context

---
+++ SpringExample1/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,16 @@
+package com.example.beans;
+
+public class Vehicle {
+	private String name;
+	//Alt + Shift + S ==> To Generate Getter and Setters
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
